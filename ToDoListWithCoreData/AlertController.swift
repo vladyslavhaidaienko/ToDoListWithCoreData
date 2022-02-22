f4dcc5d8924978226ac82c830b5ac9b50c619043
vladyslavhaidaienko <gvgnexus5@gmail.com>
2022-02-22 07:39:36 +0200

    1. StorageManager 2. CoreData

---
+++ ToDoListWithCoreData/AlertController.swift
@@ -0,0 +1,32 @@
+//
+//  AlertController.swift
+//  ToDoListWithCoreData
+//
+//  Created by Vladyslav Haidaienko on 22.02.2022.
+//
+
+import UIKit
+
+extension UIAlertController {
+    
+    static func createAlertController(withTitle title: String) -> UIAlertController {
+        UIAlertController(title: title, message: "What do you want to do?", preferredStyle: .alert)
+    }
+    
+    func action(task: Task?, completion: @escaping (String) -> Void) {
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let newValue = self.textFields?.first?.text else { return }
+            guard !newValue.isEmpty else { return }
+            completion(newValue)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField { textField in
+            textField.placeholder = "Task"
+            textField.text = task?.title
+        }
+    }
+}
---
+++ ToDoListWithCoreData/StorageManager.swift
@@ -0,0 +1,72 @@
+//
+//  StorageManager.swift
+//  ToDoListWithCoreData
+//
+//  Created by Vladyslav Haidaienko on 22.02.2022.
+//
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ToDoListWithCoreData")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - Public Methods
+    func fetchData(completion: (Result<[Task], Error>) -> Void) {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            let tasks = try viewContext.fetch(fetchRequest)
+            completion(.success(tasks))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    // Save data
+    func save(_ taskName: String, completion: (Task) -> Void) {
+        let task = Task(context: viewContext)
+        task.title = taskName
+        completion(task)
+        saveContext()
+    }
+    
+    func edit(_ task: Task, newName: String) {
+        task.title = newName
+        saveContext()
+    }
+    
+    func delete(_ task: Task) {
+        viewContext.delete(task)
+        saveContext()
+    }
+
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
